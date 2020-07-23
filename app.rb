00686df3624331ff4bc51d4383e3b7290dc5bc66
Jonathan East <jonathan_east@ymail.com>
2020-07-23 18:11:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,7 +17,13 @@ class App < Sinatra::Base
   
   get 'goodbye/:name' do
     @user_name = params[:name]
-    "Goodbye #{@user_name}"
+    "Goodbye #{@user_name}."
+  end
+  
+  get '/multiply/:num1,:num2' do
+    @total = params[:num1] * params[num2]
+    @total
+    
   end
 
 end
